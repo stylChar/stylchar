7a1af5b4c04dc19713923b4d8f83ed13fea00560
jahu5474 <jasperhuynh@Jaspers-MacBook-Air.local>
2022-08-07 17:56:37 -0700

    tailwindcss-mui-jasperjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Jasper from "./jahu5474/jasper.js";
 function App() {
   return (
     <div className="App">
       <h1> uwu i love anime waifus</h1>
+	  <Jasper/>
     </div>
   );
 }
---
+++ src/jahu5474/jasper.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+
+function Jasper() {
+
+	return (
+<>
+	<div style = {{
+		fontSize:"25px",
+		background:"red",
+	}}> stephen the stoner </div>
+	
+	<div style = {{
+		fontSize:"25px",
+		background:"gray",
+	}}>stephen the god </div>
+</>
+
+
+
+
+
+
+
+	) 
+
+
+
+}
+export default Jasper;

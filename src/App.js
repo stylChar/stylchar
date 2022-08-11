f4bd4f2713b008eb72b7e7854be9d3c86f50f444
turnertop <turnerfool@pm.me>
2022-08-10 22:42:51 -0700

    added some crazy shiii

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Jasper from "./jahu5474/jasper.js";
+import Legend from './turnertop/legend';
+import React from 'react';
+import { useState } from 'react';
+
 function App() {
   return (
     <div className="App">
       <h1> uwu i love anime waifus</h1>
 	  <Jasper/>
+    <Legend/>
     </div>
   );
 }

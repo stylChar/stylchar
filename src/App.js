b54b109031f80de2d97405cb2c407dffa73b6353
jahu5474 <jasperhuynh@Jaspers-MacBook-Air.local>
2022-08-11 18:46:25 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Jasper from "./jahu5474/jasper.js";
+import Navbar from "./jahu5474/navbar.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
 function App() {
   return (
+<>
     <div className="App">
-      <h1> uwu i love anime waifus</h1>
-	  <Jasper/>
+	  <Navbar/>
     </div>
+  
+
+	<div className="text" style={{
+		textAlign:'center',
+		
+
+	}}>
+	  <h1>hello world </h1>
+    </div>
+</>
   );
 }
-
 export default App;
